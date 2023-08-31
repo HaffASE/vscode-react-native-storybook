bff6f6a03249418347588cdfb65b2943f15a17bb
Sergei <haffase@gmail.com>
2023-08-31 21:56:44 +0300

    initial commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,160 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { createChannel } from '@storybook/channel-websocket';
 import * as vscode from 'vscode';
+import * as WebSocket from 'ws';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let storybookSocket: WebSocket;
+let storybookChannel;
+
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Started vscode-react-native-storybook');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-react-native-storybook" is now active!');
+  // FIXME: Change configuration
+  const host = 'localhost';
+  // vscode.workspace
+  //   .getConfiguration('react-native-storybooks')
+  //   .get('host');
+  const port = 7007;
+  // vscode.workspace
+  //   .getConfiguration('react-native-storybooks')
+  //   .get('port');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-react-native-storybook.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-react-native-storybook!');
-	});
+  const reactNativeUri = `ws://${host}:${port}`;
 
-	context.subscriptions.push(disposable);
+  start();
+
+  async function start() {
+    try {
+      storybookChannel = createChannel({
+        url: `ws://${host}:${port}`,
+        async: true,
+        onError: () => {},
+      });
+      storybookSocket = await connect();
+    } catch (error: unknown) {
+      throw new Error('Failed connecting to storybook server.');
+    }
+
+    storybookSocket.send(JSON.stringify({ type: 'Hello', args: {} }));
+  }
+
+  function connect(): Promise<WebSocket> {
+    return new Promise((resolve, reject) => {
+      console.log(`Connecting to ${reactNativeUri}`);
+
+      const ws = new WebSocket(reactNativeUri, {
+        perMessageDeflate: false,
+      });
+
+      const timeout = setTimeout(() => {
+        // FIXME:
+        // const error = new Error('Timed out connecting to storybook web socket');
+        // reject(error);
+      }, 10000);
+
+      const onOpen = () => {
+        console.log('Connected');
+        clearTimeout(timeout);
+        resolve(ws);
+        ws.on('message', onMessage);
+      };
+
+      const onClose = () => {
+        console.log('Connection closed');
+        clearTimeout(timeout);
+      };
+
+      const onError = (error: unknown) => {
+        console.log('Connection failed', error);
+        clearTimeout(timeout);
+        reject(error);
+        // messageQueue.rejectAll(err);
+      };
+
+      const onMessage = (data: any) => {
+        const { type, args } = JSON.parse(data);
+        console.log(
+          `Received message ${type} with args ${JSON.stringify(args, null, 2)}`,
+        );
+        // messageQueue.receiveMessage(type, args);
+      };
+
+      ws.on('open', onOpen);
+      ws.on('close', onClose);
+      ws.on('error', onError);
+    });
+  }
+
+  const registerCallbacks = () => {
+    // channel.on('setStories', data => {
+    //   const filter = vscode.workspace
+    //     .getConfiguration('react-native-storybooks')
+    //     .get('storybookFilterRegex') as string;
+    //   const regex = new RegExp(filter);
+    //   let stories: Story[] = [];
+    //   if (Array.isArray(data.stories)) {
+    //     let kinds: { [key: string]: StoryObj[] } = {};
+    //     const storydata = data.stories.filter(s => s.kind.match(regex));
+    //     storydata.map(story => {
+    //       story.stories.map(singleStory => {
+    //         if (kinds[story.kind] == undefined) {
+    //           // kinds[story.kind] = [story.name]
+    //           kinds[story.kind] = [{ name: singleStory, id: singleStory }];
+    //         } else {
+    //           kinds[story.kind].push({ name: singleStory, id: singleStory });
+    //         }
+    //       });
+    //     });
+    //     Object.keys(kinds).forEach(function (key) {
+    //       stories.push({
+    //         kind: key,
+    //         stories: kinds[key],
+    //       });
+    //     });
+    //   } else {
+    //     let kinds: { [key: string]: StoryObj[] } = {};
+    //     Object.keys(data.stories).forEach(function (key) {
+    //       const story = data.stories[key];
+    //       if (story.kind.match(regex)) {
+    //         if (kinds[story.kind] == undefined) {
+    //           // kinds[story.kind] = [story.name]
+    //           kinds[story.kind] = [{ name: story.name, id: story.id }];
+    //         } else {
+    //           kinds[story.kind].push({ name: story.name, id: story.id });
+    //         }
+    //       }
+    //     });
+    //     Object.keys(kinds).forEach(function (key) {
+    //       stories.push({
+    //         kind: key,
+    //         stories: kinds[key],
+    //       });
+    //     });
+    //   }
+    //   storiesProvider.stories = stories;
+    //   storiesProvider.refresh();
+    //   reconnectStatusBarItem.hide();
+    // });
+    // When the server in RN starts up, it asks what should be default
+    // channel.on('getCurrentStory', () => {
+    //   storybooksChannel.emit('setCurrentStory', {
+    //     storyId: currentStoryId,
+    //   });
+    // });
+    // The React Native server has closed
+    // channel.transport.socket.onclose = () => {
+    //   storiesProvider.stories = [];
+    //   storiesProvider.refresh();
+    //   reconnectStatusBarItem.show();
+    // };
+    // channel.emit('getStories');
+  };
+
+  let connectStorybook = vscode.commands.registerCommand(
+    'vscode-react-native-storybook.connectStorybook',
+    () => registerCallbacks(),
+  );
+
+  context.subscriptions.push(connectStorybook);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
